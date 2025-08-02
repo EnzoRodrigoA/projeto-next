3f2e2133f9b6d89a3b763249ee9bf4fd78deee0c
code.enzo <158048049+EnzoRodrigoA@users.noreply.github.com>
2025-08-02 12:25:26 +0000

    fix: fix imports on `eslint.config`

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,12 +1,12 @@
 // eslint.config.cjs
 const js = require("@eslint/js");
 const prettier = require("eslint-config-prettier");
-const next = require("eslint-config-next");
+const coreWebVitals = require("eslint-config-next/core-web-vitals");
 const jest = require("eslint-plugin-jest");
 
 module.exports = [
   js.configs.recommended,
-  next["core-web-vitals"],
+  coreWebVitals,
   {
     plugins: {
       jest,
