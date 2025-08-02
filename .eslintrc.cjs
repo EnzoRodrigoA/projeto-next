5eff07a3756ed71dd18445e9670b3af7f321787b
code.enzo <158048049+EnzoRodrigoA@users.noreply.github.com>
2025-08-02 12:54:18 +0000

    build: update `eslint` dependencies and fix config file

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+// .eslintrc.cjs (temporário para comparação)
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:jest/recommended",
+    "prettier",
+    "next/core-web-vitals",
+  ],
+};
